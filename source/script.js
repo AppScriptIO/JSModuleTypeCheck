cb1277c49a430d9514385f02b75ff4d5a2a3c251
Dr. Safi <sfmissive@gmail.com>
2019-05-07 02:13:45 +0300

    build

--- source/script.js
+++ source/script.js
@@ -1,20 +1,27 @@
-import filesystem from 'fs'
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.IsFileOrFolderJSModule = IsFileOrFolderJSModule;
+
+var _fs = _interopRequireDefault(require("fs"));
 
 /**
  *  Check if javascript module is a module file or directory module.
  * @return String || Boolean
  */
-export function IsFileOrFolderJSModule({
-  modulePath, // path to js module
-  isType = false, // 'file' || 'directory' - allows to return boolean in case set, respective to the type set.
+function IsFileOrFolderJSModule({
+  modulePath,
+  // path to js module
+  isType = false // 'file' || 'directory' - allows to return boolean in case set, respective to the type set.
+
 }) {
-  let moduleType
-  if (filesystem.existsSync(modulePath) && filesystem.lstatSync(modulePath).isDirectory()) moduleType = 'directory'
-  else if (filesystem.existsSync(`${modulePath}.js`) || (filesystem.existsSync(modulePath) && filesystem.lstatSync(modulePath).isFile())) moduleType = 'file'
-  else throw new Error(`Module ${modulePath} does not exist`)
+  let moduleType;
+  if (_fs.default.existsSync(modulePath) && _fs.default.lstatSync(modulePath).isDirectory()) moduleType = 'directory';else if (_fs.default.existsSync(`${modulePath}.js`) || _fs.default.existsSync(modulePath) && _fs.default.lstatSync(modulePath).isFile()) moduleType = 'file';else throw new Error(`Module ${modulePath} does not exist`); // return boolean respective to the type in question.
 
-  // return boolean respective to the type in question.
-  if (isType) return isType === moduleType
-
-  return moduleType
+  if (isType) return isType === moduleType;
+  return moduleType;
 }
