e8a5ed0720454a77cfbae8c333f9ffe5880eac72
Safi <sfmissive@gmail.com>
2019-05-07 02:11:37 +0300

    ⚗  Refactor to allow manual building and releasing of package.

--- source/script.js
+++ source/script.js
@@ -4,20 +4,17 @@ import filesystem from 'fs'
  *  Check if javascript module is a module file or directory module.
  * @return String || Boolean
  */
-export function IsFileOrFolderJSModule({ 
-    modulePath, // path to js module
-    isType = false // 'file' || 'directory' - allows to return boolean in case set, respective to the type set.
+export function IsFileOrFolderJSModule({
+  modulePath, // path to js module
+  isType = false, // 'file' || 'directory' - allows to return boolean in case set, respective to the type set.
 }) {
-    let moduleType;
-    if(filesystem.existsSync(modulePath) && filesystem.lstatSync(modulePath).isDirectory()) 
-        moduleType = 'directory'
-    else if(filesystem.existsSync(`${modulePath}.js`) || filesystem.existsSync(modulePath) && filesystem.lstatSync(modulePath).isFile())
-        moduleType = 'file'
-    else 
-        throw new Error(`Module ${modulePath} does not exist`)
+  let moduleType
+  if (filesystem.existsSync(modulePath) && filesystem.lstatSync(modulePath).isDirectory()) moduleType = 'directory'
+  else if (filesystem.existsSync(`${modulePath}.js`) || (filesystem.existsSync(modulePath) && filesystem.lstatSync(modulePath).isFile())) moduleType = 'file'
+  else throw new Error(`Module ${modulePath} does not exist`)
 
-    // return boolean respective to the type in question.
-    if(isType) return isType === moduleType
+  // return boolean respective to the type in question.
+  if (isType) return isType === moduleType
 
-    return moduleType
+  return moduleType
 }
